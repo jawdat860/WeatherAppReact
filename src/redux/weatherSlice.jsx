6f74a72e904e17ff0d1d55cd80e9c541227ae133
jawdat860 <83513860+jawdat860@users.noreply.github.com>
2024-03-22 12:54:32 +0300

    Update weatherSlice.jsx

--- src/redux/weatherSlice.jsx
+++ src/redux/weatherSlice.jsx
@@ -20,7 +20,6 @@ export const fetchWeatherData = createAsyncThunk(
       if (!save) {
         thunkAPI.dispatch(addCityToList(city));
       }
-      console.log(response.data)
       return response.data;
     } catch (error) {
       return thunkAPI.rejectWithValue(error.response.data);
